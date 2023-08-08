2010c057c151d93667a42993217d61bbf005c8e8
Utku48 <109678402+Utku48@users.noreply.github.com>
2023-08-08 17:30:39 +0300

    characterTargetMovement

--- Assets/Script/CharacterMovement.cs
+++ Assets/Script/CharacterMovement.cs
@@ -1,36 +1,29 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
+
 public class CharacterMovement : MonoBehaviour
 {
-    public Transform fallPos;
-    public Transform swimPos;
-    public Animator anim;
+    [SerializeField] private Transform _targetPos;
+    [SerializeField] private float speed;
 
-    [SerializeField] private GameObject _circle;
 
     void Start()
     {
-        _circle.SetActive(false);
-        this.gameObject.transform.DOMove(fallPos.position, 2f).OnComplete(() =>
-        {
-            // Belirli bir süre sonra ikinci hedefe hareket
-            DOVirtual.DelayedCall(1f, () =>
-            {
-                this.gameObject.transform.DOMove(swimPos.position, 2f);
-                anim.SetBool("isSwim", true);
-                _circle.SetActive(true);
-            });
-        });
+
+
     }
 
 
-    void Update()
+    void FixedUpdate()
     {
+        if (CharacterStart.Instance.swim)
+        {
+            Vector3 a = transform.position;
+            Vector3 b = _targetPos.position;
+
+            transform.position = Vector3.MoveTowards(a, b, speed);
+        }
 
     }
-    }
-
-
-
+}
---
+++ Assets/Script/CharacterStart.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+
+public class CharacterStart : MonoBehaviour
+{
+    public static CharacterStart Instance { get; private set; }
+
+    public Transform fallPos;
+    public Transform swimPos;
+    public Animator anim;
+
+    [SerializeField] private GameObject _circle;
+
+    public bool swim = false;
+    private void Awake()
+    {
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        _circle.SetActive(false);
+        this.gameObject.transform.DOMove(fallPos.position, 2f).OnComplete(() =>
+        {
+            // Belirli bir süre sonra ikinci hedefe hareket
+            DOVirtual.DelayedCall(1f, () =>
+            {
+                this.gameObject.transform.DOMove(swimPos.position, 2f);
+                anim.SetBool("isSwim", true);
+                _circle.SetActive(true);
+                swim = true;
+
+            });
+        });
+
+    }
+
+
+}
+
+
+
